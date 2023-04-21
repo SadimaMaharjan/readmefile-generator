ac236d13ff87c15fddd22f86f3e3164d605dae51
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-04-21 21:59:52 +1000

    adds questions in index.js file

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide description of the project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide the installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide the usage information",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please provide the contribution guidelines",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please provide the test instructions",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Select preferred license",
+    choices: ["MIT", "Apache-2.0", "General Public License v3.0"],
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Enter your Github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
