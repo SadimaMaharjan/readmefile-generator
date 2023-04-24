a0abc39dde2bddc1d7b90d8d8feb5408d0d7499f
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-04-24 11:48:31 +1000

    adds write to file functionality and inquirer functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -12,62 +13,72 @@ const questions = [
   {
     type: "input",
     name: "description",
-    message: "Please provide description of the project",
+    message: "Please provide description of the project: ",
   },
   {
     type: "input",
     name: "installation",
-    message: "Please provide the installation instructions",
+    message: "Please provide the installation instructions: ",
   },
   {
     type: "input",
     name: "usage",
-    message: "Please provide the usage information",
+    message: "Please provide the usage information: ",
   },
   {
     type: "input",
     name: "contributing",
-    message: "Please provide the contribution guidelines",
+    message: "Please provide the contribution guidelines: ",
   },
   {
     type: "input",
     name: "tests",
-    message: "Please provide the test instructions",
+    message: "Please provide the test instructions: ",
   },
   {
     type: "list",
     name: "license",
     message: "Select preferred license",
-    choices: ["MIT", "Apache-2.0", "ISC", "GNUPv3.0"],
+    choices: ["MIT", "ISC", "GNUPv3"],
   },
   {
     type: "input",
     name: "github",
-    message: "Enter your Github username",
+    message: "Enter your Github username: ",
   },
   {
     type: "input",
     name: "email",
-    message: "Enter your email address",
+    message: "Enter your email address: ",
   },
 ];
 
-const readMeTemplate = (questions) => ``;
-
-inquirer.prompt(questions).then((answers) => {
-  const nameOfFile = "README.md";
-  writeToFile(nameOfFile, answers);
-});
+function startProgram() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const nameOfFile = "README.md";
+      const markDownData = generateMarkdown(answers);
+      writeToFile(nameOfFile, markDownData);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, readMeTemplate(data), (err) =>
-    err ? console.log(err) : console.log("Successfully created README.md file!")
+  fs.writeFile(fileName, data, (err) =>
+    err
+      ? console.log("Could not create file")
+      : console.log("Successfully created README.md file!")
   );
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  startProgram();
+}
 
 // Function call to initialize app
 init();
